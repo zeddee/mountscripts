4f5bbe1ca258c5869687bdaea0f8dbbb708eee02
Zed Tan <zed@shootbird.work>
2019-11-01 06:21:48 +0800

    removed additional backup volume'

--- mount-ttibackupvolume.sh
+++ mount-ttibackupvolume.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 
+# mountthis2="/home/backup2/backupdir"
+# mountthis="/home/backup2/backupdir2"
+# mountpoint -q $mountthis
+# mounted=$?
+# 
+# if [[ $mounted -ne 0 ]]; then
+# 	umount $mountthis 2>&1 > /dev/null
+# 	mount /dev/sda $mountthis
+# 	chown backup2:backup2 $mountthis
+# fi
+# 
+# mountpoint -q $mountthis2
+# mounted=$?
+# 
+# if [[ $mounted -ne 0 ]]; then
+# 	umount $mountthis2 2>&1 > /dev/null
+# 	mount /dev/sdc $mountthis2
+# 	chown backup2:backup2 $mountthis2
+# fi
+# 
+
 mountthis="/home/backup2/backupdir"
-mountthis2="/home/backup2/backupdir2"
 mountpoint -q $mountthis
 mounted=$?
 
@@ -10,13 +30,3 @@ if [[ $mounted -ne 0 ]]; then
 	mount /dev/sda $mountthis
 	chown backup2:backup2 $mountthis
 fi
-
-mountpoint -q $mountthis2
-mounted=$?
-
-if [[ $mounted -ne 0 ]]; then
-	umount $mountthis2 2>&1 > /dev/null
-	mount /dev/sdb $mountthis2
-	chown backup2:backup2 $mountthis2
-fi
-
