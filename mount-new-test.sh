a470f16c7454149aa856124d2520140b4d08cc83
Zed Tan <zed@shootbird.work>
2020-07-19 18:58:50 +0200

    forgot to invert the check; inverted the check. added message for dirs alrdy mounted

--- mount-new-test.sh
+++ mount-new-test.sh
@@ -9,11 +9,13 @@ mounter() {
     DESTDIR=$2
     THISOWNER=$3
 
-    if [[ $(findmnt -M "$DESTDIR") ]]; then
+    if [[ ! $(findmnt -M "$DESTDIR") ]]; then
         echo "Mounting $DESTDIR"
         umount "$DESTDIR" 2>&1 > /dev/null # safety check
         mount "$SRCDIR" "$DESTDIR"
         chown "$THISOWNER:$THISOWNER" "$DESTDIR"
+    else
+        echo "$DESTDIR already mounted. Doing nothing."
     fi
 }
 
