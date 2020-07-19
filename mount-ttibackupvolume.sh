df1578f49c49d19ca6b37f42ae24c8611dc3718b
Zed Tan <zed@shootbird.work>
2020-07-19 19:15:06 +0200

    replacing mount scripts in mount-ttibackupvolumne.sh with updated scripts

--- mount-ttibackupvolume.sh
+++ mount-ttibackupvolume.sh
@@ -1,32 +1,47 @@
 #!/usr/bin/env bash
 
-# mountthis2="/home/backup2/backupdir"
-# mountthis="/home/backup2/backupdir2"
-# mountpoint -q $mountthis
-# mounted=$?
-# 
-# if [[ $mounted -ne 0 ]]; then
-# 	umount $mountthis 2>&1 > /dev/null
-# 	mount /dev/sda $mountthis
-# 	chown backup2:backup2 $mountthis
-# fi
-# 
-# mountpoint -q $mountthis2
-# mounted=$?
-# 
-# if [[ $mounted -ne 0 ]]; then
-# 	umount $mountthis2 2>&1 > /dev/null
-# 	mount /dev/sdc $mountthis2
-# 	chown backup2:backup2 $mountthis2
-# fi
-# 
+mounter() {
+    # 1: mount source dir
+    # 2: mount destination dir
+    # 3: owner
 
-mountthis="/home/backup2/backupdir"
-mountpoint -q $mountthis
-mounted=$?
+    SRCDIR=$1
+    DESTDIR=$2
+    THISOWNER=$3
 
-if [[ $mounted -ne 0 ]]; then
-	umount $mountthis 2>&1 > /dev/null
-	mount /dev/sda $mountthis
-	chown backup2:backup2 $mountthis
-fi
+    if [[ ! $(findmnt -M "$DESTDIR") ]]; then
+        echo "Mounting $DESTDIR"
+        umount "$DESTDIR" 2>&1 > /dev/null # safety check
+        mount "$SRCDIR" "$DESTDIR"
+        chown "$THISOWNER:$THISOWNER" "$DESTDIR"
+    else
+        echo "$DESTDIR already mounted. Doing nothing."
+    fi
+}
+
+# modified for s3fs
+mounter_s3fs() {
+    # 1: bucket name
+    # 2: mount target dir
+    # 3: s3 endpoint full url
+    # 4: Location of aws credential file. File should contain creds in the format "<key>:<secret>"
+    # 4: owner
+
+    BUCKET=$1
+    MOUNT_TARGET=$2
+    S3_URL=$3
+    PASSWORD_FILE=$4
+    THISOWNER=$5
+
+    if [[ ! $(findmnt -M "$MOUNT_TARGET") ]]; then
+        echo "Mounting $MOUNT_TARGET"
+        umount "$MOUNT_TARGET" 2>&1 > /dev/null # safety check
+        s3fs "$BUCKET" "$MOUNT_TARGET" -o url="$S3_URL",passwd_file="$PASSWORD_FILE",use_cache="/tmp",nonempty
+        chown "$THISOWNER:$THISOWNER" "$MOUNT_TARGET"
+    else
+        echo "$MOUNT_TARGET already mounted. Doing nothing."
+    fi
+}
+
+mounter "/dev/sda" "/home/backup2/backupdir" "backup2"
+mounter_s3fs "ttibackup-block" "/mnt/do-block" "https://sgp1.digitaloceanspaces.com" "/etc/.do-block-keys"
