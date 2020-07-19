373275f750e6a37a719e824ac566d3ed0c0ebb7f
Zed Tan <zed@shootbird.work>
2020-07-19 18:23:56 +0200

    abstracted check mount logic

--- mount-new-test.sh
+++ mount-new-test.sh
@@ -1,12 +1,22 @@
+check_mount() {
+    # 1: Check if this dir is mounted
+    if [[ $(findmnt -M "$1") ]]; then
+        return true
+    else
+        return false
+    fi
+}
+
 auto_mount() {
     # 1: mount source dir
     # 2: mount destination dir
     # 3: owner
-    THISOWNER=${3:backup2}
+
     SRCDIR=$1
     DESTDIR=$2
-    if [[ ! $(findmnt -M "$DESTDIR") ]]; then
-        owner?=
+    THISOWNER=${3:backup2}
+
+    if [[ ! check_mount "$DESTDIR" ]]; then
         umount "$DESTDIR" 2>&1 > /dev/null # safety check
         mount "$SRCDIR" "$DESTDIR"
         chown "$THISOWNER:$THISOWNER" "$DESTDIR"
